8de942dc261b08286e9dcd19ec88593fb36c4502
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-06-07 11:53:08 +0100

    fetch with exact order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,17 +2,23 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+const out = []
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
     if (error) {
         console.log(error);
     }
     const data = JSON.parse(body);
-    for (const character of data.characters) {
-        request(character, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            }
-            console.log(JSON.parse(body).name);
-        });
-    }
+    fetchCharacters(data.characters, 0);
 });
+
+function fetchCharacters (characters, index) {
+    request(characters[index], function (error, response, body) {
+        if (error) {
+            return
+        }
+        console.log(JSON.parse(body).name);
+        if (index < characters.length - 1) {
+            fetchCharacters(characters, index + 1);
+        }
+    });
+}
