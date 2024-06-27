f5ace26ca084df0866d4d660a55ee4dc72247f88
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-06-27 11:07:41 +0100

    calculate the island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Calculate the island perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    grid is a list of list of integers ( [[0, 0, 0, 0, 0, 0], [0, 1, 0, 1, 1, 0], [0, 1, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0]])
+    """
+    if grid is None:
+        return 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
